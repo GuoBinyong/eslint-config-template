41c03a63468d883990f30376dc3d404d365e136a
郭斌勇 <guobinyong@qq.com>
2020-05-15 11:38:22 +0800

    增加JavaScript配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly",
         "globalThis":"readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
     },
     "rules": {
         // 允许 debugger 运行在 开发 环境中
